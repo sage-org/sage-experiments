d0b6b6e7885b653a44b55e09c7bcfc9300578849
Thomas Minier <tminier01@gmail.com>
2019-04-26 13:31:10 +0200

    update reset_db script

--- scripts/update/reset_db_state.sh
+++ scripts/update/reset_db_state.sh
@@ -5,6 +5,7 @@
 BIGBOSS_USERNAME='ladda'
 BIGBOSS_IP='172.16.8.50'
 START_DB_SCRIPT='./postgre_backup/run_postgre.sh'
+START_SAGE_SCRIPT='sage-postgre'
 SAGE_CONTAINER_NAME='sage-postgres'
 POSTGRES_CONTAINER_NAME='some-postgres'
 
@@ -13,8 +14,11 @@ POSTGRES_USER='minier-t'
 POSTGRES_DB='minier-t'
 
 # stop SaGe server
+# we need to completely remove the container as the link with the postgre container
+# will be broken when re-building it.
 SAGE_PID=`ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} docker ps -aqf "name=${SAGE_CONTAINER_NAME}"`
 ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} docker kill ${SAGE_PID}
+ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} docker rm ${SAGE_PID}
 
 # restart PostgreSQL DB
 # we need to kill, delete and recreate the whole container
@@ -30,7 +34,7 @@ ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} ${START_DB_SCRIPT}
 until ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} docker run --rm --link ${POSTGRES_CONTAINER_NAME}:pg postgres pg_isready -U $POSTGRES_USER -h pg; do sleep 10; done
 
 # restart SaGe server
-ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} docker restart ${SAGE_PID}
+ssh ${BIGBOSS_USERNAME}@${BIGBOSS_IP} ${START_SAGE_SCRIPT}
 
 # sleep another 10 seconds, for the sake of safety (I do not trust this script at 100% )
 sleep 10
